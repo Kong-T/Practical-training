11b579784ebdabee14db4e9866464db9c4c8dfe0
kong-T <1812536092@qq.com>
2020-11-29 18:27:04 +0800

    填写部分链接和footer测试

---
+++ website_tan/js/login.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+        $(function () {
+            $('#btn').click(function (e) {
+                window.location.href = "..\..\web\bord&notice\notice1.html";
+            });
+        });
+</script>
